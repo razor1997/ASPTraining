0862a11404b7d72d5609391305f5fbf1d50fcd46
Konrad Bielak <konrad.bielak@pollub.edu.pl>
2021-06-30 23:00:01 +0200

    Added login page for users

---
+++ ASPTraining.Web/Pages/UserPage.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPTraining.Web.Pages
+{
+    public class UserPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
