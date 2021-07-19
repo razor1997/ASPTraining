d3dd65d7cae57edf1a5a9427d5dae6e61d06efd1
Konrad Bielak <konrad.bielak@pollub.edu.pl>
2021-07-19 22:44:45 +0200

    Add bootstrap to project, new page (loginPage)

---
+++ ASPTraining.Web/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPTraining.Web.Models;
+using System.Data.SqlClient;
+
+namespace ASPTraining.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        //SqlConnection connection = new SqlConnection();
+        //SqlCommand command = new SqlCommand();
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        void ConnectionString()
+        {
+
+        }
+        public ActionResult Verify(Account account)
+        {
+            ConnectionString();
+        
+            return View();
+        }
+    }
+}
